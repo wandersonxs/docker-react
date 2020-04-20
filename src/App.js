92ca7e6e96515a69e66bb6afbb56c2ddcacc7b5b
Wanderson Xesquevixos de Siqueira <wandersonxs@gmail.com>
2020-04-19 21:19:00 -0300

    Updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there! We are at ElasticBeanstalk Wanderson! Great!!!!!!
+            Hi there! We are at ElasticBeanstalk Wanderson Xesquevixos! Great!!!!!!
           </p>
           <a
             className="App-link"
